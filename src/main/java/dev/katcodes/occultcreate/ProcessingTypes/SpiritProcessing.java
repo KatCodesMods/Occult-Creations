40f396829ea43037aa841a2f0987fb6381a84990
Kat Gaea <kat.knight@newgaea.net>
2024-02-21 10:51:00 +0000

    feat: Add initial (non-working) skeleton

---
+++ src/main/java/dev/katcodes/occultcreate/ProcessingTypes/SpiritProcessing.java
@@ -0,0 +1,10 @@
+package dev.katcodes.occultcreate.ProcessingTypes;
+
+import com.simibubi.create.content.kinetics.fan.processing.FanProcessing;
+
+public class SpiritProcessing  extends FanProcessing {
+
+
+
+     
+}
